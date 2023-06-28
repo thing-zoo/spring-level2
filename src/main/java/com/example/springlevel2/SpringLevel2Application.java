ee9107184d8c08bc5a63d3d172c7282969b33d88
thing-zoo <cathy4025@gmail.com>
2023-06-28 19:43:02 +0900

    Initial commit

---
+++ src/main/java/com/example/springlevel2/SpringLevel2Application.java
@@ -0,0 +1,13 @@
+package com.example.springlevel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLevel2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLevel2Application.class, args);
+    }
+
+}
